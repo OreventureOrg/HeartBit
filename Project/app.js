9feb285db1c8ac1c9ff74bb5a8e13c3ccaa12d0a
Miguel Brandão <114339832+MiguelbProductions@users.noreply.github.com>
2024-06-14 16:00:23 +0100

    Update

---
+++ Project/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/heartbit', {});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Erro de conexão com MongoDB:'));
+db.once('open', () => {
+    console.log('Conectado ao MongoDB');
+});
+
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) => {
+    res.render("index.html", { Page: "Home"});
+});
+
+app.get("/news", (req, res) => {
+    res.render("./news/news-list.html", { Page: "News"});
+});
+
+app.get("/new/:id", (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    res.render("./news/news-details.html", { Page: "New Details"});
+});
+
+
+const userRoutes = require('./src/routes/userRoutes');
+app.use('/', userRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
---
+++ Project/src/controllers/userController.js
@@ -0,0 +1,36 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const registerUser = async (req, res) => {
+    const { username, email, password, firstName, lastName } = req.body;
+    try {
+        const user = new User({ username, email, password, firstName, lastName });
+        await user.save();
+        res.status(201).json({ message: 'Usuário registrado com sucesso' });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: 'Usuário não encontrado' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Senha incorreta' });
+        }
+
+        const token = jwt.sign({ id: user._id }, 'secreta', { expiresIn: '1h' });
+        res.status(200).json({ token });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+module.exports = { registerUser, loginUser };
